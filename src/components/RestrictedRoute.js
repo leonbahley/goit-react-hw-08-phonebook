4ebcb1e6dd15c116b2f1de9f226cfd44957b3784
leonbahley <leon.bahley@icloud.com>
2022-12-10 23:56:22 +0100

    Update RestrictedRoute.js

--- src/components/RestrictedRoute.js
+++ src/components/RestrictedRoute.js
@@ -1,7 +1,5 @@
 import { useAuth } from 'hooks/useAuth';
 import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from 'redux/auth/selectors';
 
 export const RestrictedRoute = ({ component, redirectTo = '/' }) => {
   const { isLoggedIn } = useAuth();
